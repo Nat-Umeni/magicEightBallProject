94e996e3fb225e58409069d1447638b8d38d2f43
MrNat <itsnat381@gmail.com>
2023-12-21 19:58:55 +0000

    Added random number generator, to get 3 random words from 3 diffferent arrays of nouns adjectives and adverbs. Program should print a random message to console, for future can add more adjectives or word types to create a random story generator

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-const fortuneButton = document.getElementById('getFortuneButton');
+//Word arrays
 
+
+const adjectives = ['slimy', 'warm', 'vague', 'sticky', 'polite'];
+
+const adverbs = ['dramatically', 'angrily', 'obnoxiously', 'worriedly', 'coyly'];
+
+const nouns = ['oven', 'cow', 'man', 'monster', 'potato'];
+
+
+
+const adjIndex = () => {
+    return Math.floor(Math.random() * 5);
+};
+
+const advIndex = () => {
+    return Math.floor(Math.random() * 5);
+};
+
+const nounIndex = () => {
+    return Math.floor(Math.random() * 5);
+};
+
+
+let chosenAdj = adjectives[adjIndex()];
+
+let chosenAdv = adverbs[advIndex()];
+
+let chosenNoun = nouns[nounIndex()];
+
+/* To check that the indexes are being generated correctly
+console.log(chosenAdj);
+console.log(chosenAdv);
+console.log(chosenNoun); */
+
+console.log(`There once was a ${chosenNoun}, who was ${chosenAdj}, and could not help itself from staring ${chosenAdv}`);
