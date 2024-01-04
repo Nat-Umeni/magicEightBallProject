207232180b3acdec47fae586f671d1c550d0583f
MrNat <itsnat381@gmail.com>
2024-01-04 16:25:28 +0000

    Refactored random index code/assignment to be more readable and easier to handle. Started work on adding a new sentence generator too

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-//Word arrays
+//Word arrays (Need to add more words and change the sentence each time the program is ran, started on line 10)
 
 
-const adjectives = ['slimy', 'warm', 'vague', 'sticky', 'polite'];
+const adjectives = ['slimy', 'warm', 'vague', 'sticky', 'polite', 'handsome', 'beautiful'];
 
 const adverbs = ['dramatically', 'angrily', 'obnoxiously', 'worriedly', 'coyly'];
 
-const nouns = ['oven', 'cow', 'man', 'monster', 'potato'];
+const nouns = ['oven', 'cow', 'man', 'monster', 'potato', 'woman'];
+
+/*  let sentence = ''
+    let sentenceArray = [`There once was a ${chosenNoun}, who was ${chosenAdj}, and could not help itself from staring ${chosenAdv}`, `Soon I will be the most ${chosenAdj} ${chosenNoun} that has ever {chosenAdv} lived.`,];
 
 
 
-const adjIndex = () => {
-    return Math.floor(Math.random() * 5);
+*/
+
+
+
+//generates a random index depending on the length of the word array entered into it
+const giveRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length);
 };
 
-const advIndex = () => {
-    return Math.floor(Math.random() * 5);
-};
+//assiging random indexes to arrays
 
-const nounIndex = () => {
-    return Math.floor(Math.random() * 5);
-};
+let adjIndex = giveRandomIndex(adjectives);
 
+let advIndex = giveRandomIndex(adverbs);
 
-let chosenAdj = adjectives[adjIndex()];
+let nounIndex = giveRandomIndex(nouns);
 
-let chosenAdv = adverbs[advIndex()];
+//Chosen words from arrays are assined below
 
-let chosenNoun = nouns[nounIndex()];
+let chosenAdj = adjectives[adjIndex];
+
+let chosenAdv = adverbs[advIndex];
+
+let chosenNoun = nouns[nounIndex];
+
 
 /* To check that the indexes are being generated correctly
+
 console.log(chosenAdj);
 console.log(chosenAdv);
-console.log(chosenNoun); */
+console.log(chosenNoun); 
+
+*/
 
 console.log(`There once was a ${chosenNoun}, who was ${chosenAdj}, and could not help itself from staring ${chosenAdv}`);
